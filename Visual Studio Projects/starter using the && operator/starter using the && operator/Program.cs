7f29682baf98778b0987439e154fa9bd9117fe36
561416-spec <561416@escg.ac.uk>
2026-01-08 19:41:36 +0000

    1

---
+++ Visual Studio Projects/starter using the && operator/starter using the && operator/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace starter_using_the____operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool choicebool1 = false;
+            bool choicebool2 = false;
+
+            Console.WriteLine("Select your first true or false");
+            Console.WriteLine();
+            Console.WriteLine("[1]. True");
+            Console.WriteLine("[2]. False");
+            Console.WriteLine();
+            Console.Write("Enter your decision here: ");
+            int choice1 = Convert.ToInt32(Console.ReadLine());
+            if (choice1 == 1)
+            {
+                choicebool1 = true;
+            }
+            else if (choice1 == 2)
+            {
+                choicebool1 = false;
+            }
+            Thread.Sleep(500);
+            Console.Clear();
+
+            Console.WriteLine("Select your second true or false");
+            Console.WriteLine();
+            Console.WriteLine("[1]. True");
+            Console.WriteLine("[2]. False");
+            Console.WriteLine();
+            Console.Write("Enter your decision here: ");
+            int choice2 = Convert.ToInt32(Console.ReadLine());
+            if (choice2 == 1)
+            {
+                choicebool2 = true;
+            }
+            else if (choice2 == 2)
+            {
+                choicebool2 = false;
+            }
+            Thread.Sleep(500);
+            Console.Clear();
+
+            Console.WriteLine(choicebool1 && choicebool2);
+        }
+    }
+}
